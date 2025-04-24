4a36b4e69d89de7ab9912a2003836b5902094291
Abbhgkohhhh <saiid9104@gmail.com>
2025-04-24 20:58:28 +0330

    Update start_handler.py

--- start_handler.py
+++ start_handler.py
@@ -1,6 +1,1 @@
-# handlers/start_handler.py
-from telegram import Update
-from telegram.ext import ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! برای افزودن پیج از دستور /add استفاده کن.")
