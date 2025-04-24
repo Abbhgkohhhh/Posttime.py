0e7df629d4b86b81f438909a59c6311f4839f14a
Abbhgkohhhh <saiid9104@gmail.com>
2025-04-24 20:57:12 +0330

    Update models.py

--- models.py
+++ models.py
@@ -1,12 +1,1 @@
-# data/models.py
-from dataclasses import dataclass, field
 
-@dataclass
-class Page:
-    user_id: int
-    username: str
-    category: str
-    desc: str
-    products: str
-    verified: bool = False
-    scores: list = field(default_factory=list)
